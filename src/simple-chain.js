495f66f6f34f54ea22b8740352729b7e6c7bea3d
Altynay <altynaygoha2007@mail.ru>
2022-05-01 10:26:10 +0600

    "added 2 tasks"

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+   chains: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+     return this.chains.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chains.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+           const index = position - 1;
+           if (this.chains[index] === undefined) {
+             this.chains = [];
+             throw new Error("You can't remove incorrect link!");
+           }
+           this.chains.splice(index, 1);
+           return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+       this.chains.reverse();
+       return this;
+
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+       let finish = this.chains.join("~~");
+       this.chains.length = 0;
+       return finish;
+
   }
 };
 
